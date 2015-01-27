a7bd52c8ec88d62961a208ae0a6d675ba1a27a34
Garry Yao <yaojun85@gmail.com>
2015-01-27 14:30:26 +0800

    recorder service now return a self-starting promise instead

--- examples/main.js
+++ examples/main.js
@@ -75,14 +75,19 @@ require.config({
     }
   },
   callback: function loadDeps() {
-    require([
-      'jquery',
-      'troopjs-dom/application/widget',
-      'example/widget/main',
-      'example/widget/volume'
-    ], function Bootstrap(jQuery, Application) {
-      jQuery(function ready($) {
+  require([
+    'jquery',
+    'troopjs-recorder/service/recorder',
+    'troopjs-dom/application/widget',
+    'example/widget/main',
+    'example/widget/volume'
+  ], function Bootstrap(jQuery, record_service, Application) {
+    jQuery(function ready($) {
+
+      record_service.then(function () {
         Application($('html'), 'bootstrap').start();
+      }).otherwise(function (err) {
+        $('body').html('Unsupported ' + (('browser' in err.reason) ? 'browser' : 'Flash version'));
       });
     });
   }
